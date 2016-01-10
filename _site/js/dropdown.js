44b873b81b05951c0fb4f5eb6300dcd3ae028fa7
Arturas Leonovas <arthurleonov@gmail.com>
2016-01-10 22:30:03 +0000

    commit

--- _site/js/dropdown.js
+++ _site/js/dropdown.js
@@ -3,7 +3,7 @@ var dropdown = function() {
     submenus : '.dropdown-menu-custom',
     customDropdown: '.custom-dropdown',
     dropdownLink: '.dropdown-menu-custom li',
-    button: '.select-button'
+    button: '.btn'
   };
   return {
     init: function (configuration) {
@@ -14,8 +14,7 @@ var dropdown = function() {
       dropdown.showHideDropdown();
       dropdown.updateValue();
     },
-    showHideDropdown: function()
-    {
+    showHideDropdown: function() {
 
         $.each($(defaults.customDropdown), function(index, element)
         {
@@ -30,14 +29,14 @@ var dropdown = function() {
 
                 tl.from(subMenu, .2, {
                     autoAlpha: 0,
-                    y:         '-10',
+                    y: '-10',
                 });
             }
 
             element.subMenuAnimation = tl;
 
-            $(element).on('click', function()
-            {
+            $(element).on('click', function() {
+
                 var isActive = $(element).hasClass('active');
 
                 // Close other dropdowns
@@ -62,18 +61,6 @@ var dropdown = function() {
             });
 
         });
-
-      // function menuItemOver()
-      // {
-      //   this.subMenuAnimation.play();
-      //   $(this).one("click", menuItemOut);
-      // }
-
-      // function menuItemOut()
-      // {
-      //   this.subMenuAnimation.reverse();
-      //   $(this).one("click", menuItemOver);
-      // }
     },
     updateValue: function(){
       $(defaults.dropdownLink).click(function () {
@@ -83,10 +70,3 @@ var dropdown = function() {
     }
   };
 }();
-
-
-// 1. Custon dropdown that slides out with a nice animation on click DONE
-//    1.1 animation preferably tweenmax DONE
-// 2. if you click on the object or away the animation reverses itself and goes away DONE
-// 3. it only works on this element not every element
-// 4. when I click on the dropdown, the parent container updates the value
